92761323302451050d36489b9bdadf48f00991d1
Daniel Nolan <DanNolan99@gmail.com>
2020-11-09 11:44:16 -0500

    update argument order

--- app/index.js
+++ app/index.js
@@ -8,8 +8,8 @@ async function newContract() {
   const beneficiary = document.getElementById("beneficiary").value;
   const arbiter = document.getElementById("arbiter").value;
   const value = ethers.BigNumber.from(document.getElementById("wei").value);
-  const contract = await deploy(beneficiary, arbiter, value);
-  addContract(++contracts, contract, beneficiary, arbiter, value);
+  const contract = await deploy(arbiter, beneficiary, value);
+  addContract(++contracts, contract, arbiter, beneficiary, value);
 }
 
 document.getElementById("deploy").addEventListener("click", newContract);
