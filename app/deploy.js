e6595c2d14acc71a9275a217b431a067d1628557
Daniel Nolan <DanNolan99@gmail.com>
2021-02-17 08:40:18 -0500

    replace ethereum.enable

--- app/deploy.js
+++ app/deploy.js
@@ -4,7 +4,7 @@ import {ethers} from 'ethers';
 const provider = new ethers.providers.Web3Provider(ethereum);
 
 export default async function deploy(arbiter, beneficiary, value) {
-  await ethereum.enable();
+  await ethereum.request({ method: 'eth_requestAccounts' });
   const signer = provider.getSigner();
   const factory = new ethers.ContractFactory(Escrow.abi, Escrow.bytecode, signer);
   return factory.deploy(arbiter, beneficiary, { value });
